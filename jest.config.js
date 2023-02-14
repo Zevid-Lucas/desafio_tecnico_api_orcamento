b7ed56fd6508805672b1c1f51d8a7bcffb984afe
David Costa <zevid.dev@outlook.com>
2023-02-14 11:36:11 -0300

    🐳 chore:add config jest

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   roots: ["<rootDir>/tests"],
-  collectCoverageFrom: ["<rootDir>/tests/**/*.js"],
+  collectCoverageFrom: ["<rootDir>/tests/**/*.ts"],
   coverageDirectory: "coverage",
   testEnvironment: "node",
   transform: {
