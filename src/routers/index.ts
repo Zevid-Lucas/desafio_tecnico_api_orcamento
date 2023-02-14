5e6b2f3662e1175ca1b016de5a8231a54f00109e
David Costa <zevid.dev@outlook.com>
2023-02-14 11:34:31 -0300

    🐳 chore:create barrel for routes

---
+++ src/routers/index.ts
@@ -0,0 +1,5 @@
+import usersRoute from "./users.routes";
+import productsRoute from "./products.routes";
+import userAndProductsRoutes from "./userAndProducts.routes";
+
+export { usersRoute, productsRoute, userAndProductsRoutes };
