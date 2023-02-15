239c9137f881bbd7336e0156bdcbd7c991b853a3
David Costa <zevid.dev@outlook.com>
2023-02-15 18:36:09 -0300

    🦄 refactor:change endpoint of camelCase for snake_case

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ const app = express();
 
 app.use("/users", routes.usersRoute);
 app.use("/products", routes.productsRoute);
-app.use("/usersAndProducts", routes.userAndProductsRoutes);
+app.use("/user_products", routes.userAndProductsRoutes);
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 export default app;
