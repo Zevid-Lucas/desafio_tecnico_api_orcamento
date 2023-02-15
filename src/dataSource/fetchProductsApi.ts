9a0fd1b7a19be1270a7c4de75a9d76bd27feba5f
David Costa <zevid.dev@outlook.com>
2023-02-15 10:45:35 -0300

    🦄 refactor:remove try/catch

--- src/dataSource/fetchProductsApi.ts
+++ src/dataSource/fetchProductsApi.ts
@@ -3,23 +3,15 @@ import axios from "axios";
 const BASE_URL = "https://mockend.com/juunegreiros/BE-test-api/";
 
 async function fetchProducts() {
-  try {
-    const response = await axios.get(`${BASE_URL}products`);
-    const { data } = response;
-    return data;
-  } catch (error) {
-    console.error(error);
-  }
+  const response = await axios.get(`${BASE_URL}products`);
+  const { data } = response;
+  return data;
 }
 
 async function fetchProductsForId(productsIds: string) {
-  try {
-    const response = await axios.get(`${BASE_URL}products/${productsIds}`);
-    const { data } = response;
-    return data;
-  } catch (AxiosError) {
-    console.error(AxiosError);
-  }
+  const response = await axios.get(`${BASE_URL}products/${productsIds}`);
+  const { data } = response;
+  return data;
 }
 
 export default { fetchProducts, fetchProductsForId };
--- src/dataSource/fetchUserAndProducts.ts
+++ src/dataSource/fetchUserAndProducts.ts
@@ -2,19 +2,15 @@ import fetchProductsApi from "./fetchProductsApi";
 import fetchUsersApi from "./fetchUsersApi";
 
 async function fetchUserAndProducts(userId: string, productsIds: string) {
-  try {
-    const userResponse = await fetchUsersApi.fetchUserForId(userId);
-    const productsResponse = await fetchProductsApi.fetchProductsForId(
-      productsIds
-    );
+  const userResponse = await fetchUsersApi.fetchUserForId(userId);
+  const productsResponse = await fetchProductsApi.fetchProductsForId(
+    productsIds
+  );
 
-    return {
-      userResponse,
-      productsResponse,
-    };
-  } catch (error) {
-    console.error(error);
-  }
+  return {
+    userResponse,
+    productsResponse,
+  };
 }
 
 export default fetchUserAndProducts;
